a7cd4bb8d65dcf989f8d23ef37e0674c7a353de2
MHGameOnline <gamechangerboss123@gmail.com>
2026-01-09 17:11:26 +0530

    Create game-data.js

---
+++ game-data.js
@@ -0,0 +1,152 @@
+const games = [
+  {
+    name: "RAJA-RANI MORNING",
+    result: "677-08-233",
+    panel: "https://sattamatkadpboss.co/record/raja-rani-morning-panel-chart.php",
+    lucky: [4, 7, 9]
+  },
+  {
+    name: "SITA MORNING",
+    result: "238-31-489",
+    panel: "https://dpbosse.net/pannel/SITAMORNING/81",
+    lucky: [1, 3, 8]
+  },
+  {
+    name: "KARNATAKA DAY",
+    result: "478-93-490",
+    panel: "https://sattamatkadpboss.co/karnataka-day-panel-chart.php",
+    lucky: [2, 6, 9]
+  },
+  {
+    name: "KALYAN MORNING",
+    result: "350-81-489",
+    panel: "https://sattamatkadpboss.co/record/kalyan-morning-penal-chart.php",
+    lucky: [0, 5, 8]
+  },
+  {
+    name: "MADHUR MORNING",
+    result: "580-38-477",
+    panel: "https://sattamatkadpboss.co/madhur-morning-penal-chart.php",
+    lucky: [3, 7, 9]
+  },
+  {
+    name: "SRIDEVI",
+    result: "280-08-468",
+    panel: "https://sattamatkadpboss.co/record/sridevi-satta-penal-chart.php",
+    lucky: [0, 6, 8]
+  },
+  {
+    name: "MAHARANI",
+    result: "568-98-369",
+    panel: "https://sattamatkadpboss.co/maharani-panel-chart.php",
+    lucky: [5, 8, 9]
+  },
+  {
+    name: "TIME BAZAR",
+    result: "899-65-140",
+    panel: "https://sattamatkadpboss.co/time-bazar-panel-chart.php",
+    lucky: [1, 4, 9]
+  },
+  {
+    name: "SITA DAY",
+    result: "700-76-240",
+    panel: "https://dpbosse.net/pannel/SITADAY/89",
+    lucky: [0, 2, 7]
+  },
+  {
+    name: "MADHUR DAY",
+    result: "133-70-677",
+    panel: "https://sattamatkadpboss.co/madhur-day-panel-chart.php",
+    lucky: [3, 6, 7]
+  },
+  {
+    name: "MILAN DAY",
+    result: "479-00-136",
+    panel: "https://sattamatkadpboss.co/record/milan-day-penal-chart.php",
+    lucky: [0, 1, 9]
+  },
+  {
+    name: "RAJDHANI DAY",
+    result: "239-48-378",
+    panel: "https://sattamatkadpboss.co/record/rajdhani-day-penal-chart.php",
+    lucky: [2, 3, 8]
+  },
+  {
+    name: "SUPREME DAY",
+    result: "230-59-270",
+    panel: "https://sattamatkadpboss.co/supreme-day-panel-chart.php",
+    lucky: [0, 5, 7]
+  },
+  {
+    name: "KALYAN",
+    result: "367-65-168",
+    panel: "https://sattamatkadpboss.co/record/kalyan-penal-chart.php",
+    lucky: [3, 6, 8]
+  },
+  {
+    name: "MAHARANI DAY",
+    result: "150-61-119",
+    panel: "https://sattamatkadpboss.co/maharani-day-panel-chart.php",
+    lucky: [1, 5, 9]
+  },
+  {
+    name: "SITA NIGHT",
+    result: "129-20-578",
+    panel: "https://dpbosse.net/pannel/SITANIGHT/109",
+    lucky: [2, 5, 9]
+  },
+  {
+    name: "KARNATAKA NIGHT",
+    result: "345-24-239",
+    panel: "https://sattamatkadpboss.co/karnataka-night-panel-chart.php",
+    lucky: [3, 4, 9]
+  },
+  {
+    name: "SRIDEVI NIGHT",
+    result: "689-36-123",
+    panel: "https://sattamatkadpboss.co/record/sridevi-night-satta-penal-chart.php",
+    lucky: [1, 3, 6]
+  },
+  {
+    name: "SUPREME NIGHT",
+    result: "890-73-599",
+    panel: "https://sattamatkadpboss.co/supreme-night-panel-chart.php",
+    lucky: [7, 8, 9]
+  },
+  {
+    name: "MADHUR NIGHT",
+    result: "166-33-139",
+    panel: "https://sattamatkadpboss.co/madhur-night-panel-chart.php",
+    lucky: [1, 3, 6]
+  },
+  {
+    name: "MILAN NIGHT",
+    result: "225-98-440",
+    panel: "https://sattamatkadpboss.co/record/milan-night-penal-chart.php",
+    lucky: [2, 4, 9]
+  },
+  {
+    name: "RAJDHANI NIGHT",
+    result: "355-34-338",
+    panel: "https://sattamatkadpboss.co/record/rajdhani-night-penal-chart.php",
+    lucky: [3, 5, 8]
+  },
+  {
+    name: "KALYAN NIGHT",
+    result: "468-81-579",
+    panel: "https://sattamatkadpboss.co/record/kalyan-night-penal-chart.php",
+    lucky: [4, 7, 9]
+  },
+  {
+    name: "MAIN BAZAR",
+    result: "499-21-678",
+    panel: "https://sattamatkadpboss.co/main-bazar-panel-chart.php",
+    lucky: [1, 6, 9]
+  },
+  {
+    name: "MAHARANI NIGHT",
+    result: "355-32-156",
+    panel: "https://sattamatkadpboss.co/maharani-night-panel-chart.php",
+    lucky: [3, 5, 6]
+  }
+];
